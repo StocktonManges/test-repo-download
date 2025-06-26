6b72f3a82ff1db32982658274451ef3ce242e1cb
stockton.manges <stockton.manges@escrowtech.com>
2025-06-26 12:51:42 -0600

    Update ignored files list to include your_mom.txt and a new path for your dad

--- scripts/run-workflow.ts
+++ scripts/run-workflow.ts
@@ -49,7 +49,7 @@ async function triggerWorkflow() {
 
     console.log('Triggering workflow...');
 
-    const ignored_files = ['*.git*', 'your mom/*'];
+    const ignored_files = ['*.git*', '*/your dad/*', 'your_mom.txt'];
 
     return await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
         owner: OWNER,
